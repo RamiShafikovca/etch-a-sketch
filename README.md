3c5085bef160fa3d23090d5b4a2c49e101905bf2
Your Name <rami@shafikov.ca>
2022-08-07 16:13:59 -0400

    //Add theme, sidebar for colors, readme

---
+++ README.md
@@ -0,0 +1,15 @@
+#Etch-a-Sketch
+Sketchpad webpage for doodling
+
+Created with HTML, CSS, and JavaScript
+
+**Github Pages Preview** [here](https://ramishafikovca.github.io/etch-a-sketch)
+
+##Features:
+Specify your canvas size from a slider
+
+4 color modes: Black, Rainbow, Gruvbox, and Random
+
+Reset/clear the canvas
+
+ ![Screenshot of the page](screenshot.png)
--- main.js
+++ main.js
@@ -1,24 +1,50 @@
 const gridContainer = document.getElementById('gridContainer');
-const GRIDSIZE = 400;
-gridContainer.setAttribute('style', `height: ${GRIDSIZE}px`);
-gridContainer.setAttribute('style', `width: ${GRIDSIZE}px`);
 const promptSlider = document.querySelector('input');
-const sliderValue = document.querySelector('p#sliderValue');
+const sliderValue = document.querySelector('div#sliderValue');
+const clearBtn = document.querySelector('button.clearGrid');
 const colors = ['violet', 'indigo', 'blue', 'green', 'yellow', 'orange', 'red'];
+const gruvColors = ['#cc241d', '#98971a', '#d79921', '#458588', '#b16286', '#689d6a', '#d65d0e'];
+const GRIDSIZE = 500;
 let colorCount = 0;
+
+gridContainer.style.height = `${GRIDSIZE}px`;
+gridContainer.style.width = `${GRIDSIZE}px`;
 
 generateGrid();
 
-function generateGrid (GRIDCOUNT = 16) {
-    let ITEMSIZE = GRIDSIZE / GRIDCOUNT;
-    for (let i = 0; i < GRIDCOUNT; i++) {
+//Event listener for displaying grid size
+promptSlider.addEventListener('input', () => {
+    sliderValue.innerHTML = `Size: ${promptSlider.value} x ${promptSlider.value}`;
+
+});
+
+//Re-generate the grid after new grid size is selected
+promptSlider.addEventListener('mouseup', () => {
+    generateGrid(promptSlider.value);
+
+});
+
+//Select each gridItem and reset their background color
+clearBtn.addEventListener('click', () => {
+    document.querySelectorAll('div.gridItem').forEach((item) => {
+        item.style.backgroundColor = '';
+    });
+});
+
+//Clear grid and then create and add specified number of grid items
+function generateGrid (gridCount = promptSlider.value) {
+    while (gridContainer.lastChild) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+    let ITEMSIZE = GRIDSIZE / gridCount;
+    for (let i = 0; i < gridCount; i++) {
         let gridRow = document.createElement('div');
         gridRow.classList.add('gridRow');
         gridContainer.append(gridRow);
 
-        for (let j = 0; j < GRIDCOUNT; j++) {
+        for (let j = 0; j < gridCount; j++) {
             let gridItem = document.createElement('div');
-            gridItem.setAttribute('style', `height: ${ITEMSIZE}px; aspect-ratio: 1 / 1`);
+            gridItem.style = (`height: ${ITEMSIZE}px; aspect-ratio: 1 / 1; draggable=false`);
             gridItem.classList.add('gridItem');
             gridRow.append(gridItem);
         }
@@ -26,27 +52,27 @@ function generateGrid (GRIDCOUNT = 16) {
     addGridListeners();
 }
 
-
+//Add listeners to each item that will color when the cursor passes through it
 function addGridListeners () {
     const gridItems = document.querySelectorAll('div.gridItem');
     gridItems.forEach((item) => {
-        item.addEventListener('mouseout', () =>{
-            item.setAttribute('id', `${colors[colorCount]}`);
-            colorCount++;
-            colorCount = colorCount % 7;
+        item.addEventListener('mouseenter', () =>{
+            fillGridItem(item);
         });
     });
 }
 
-promptSlider.addEventListener('input', () => {
-    sliderValue.innerHTML = `${promptSlider.value} x ${promptSlider.value}`;
-
-});
-
-promptSlider.addEventListener('mouseup', () =>{
-    while (gridContainer.lastChild) {
-        gridContainer.removeChild(gridContainer.lastChild);
+//Fill by user-selected color scheme
+function fillGridItem (item) {
+    if (document.getElementById('black').checked) {
+        item.style.backgroundColor = 'black';
+    } else if (document.getElementById('rainbow').checked) {
+        item.style.backgroundColor = colors[colorCount];
+        colorCount = (colorCount + 1) % 7;
+    } else if (document.getElementById('gruvbox').checked) {
+        item.style.backgroundColor = gruvColors[colorCount];
+        colorCount = (colorCount + 1) % 7;
+    } else if (document.getElementById('random').checked) {
+        item.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
     }
-    generateGrid(promptSlider.value);
-
-});
+}
